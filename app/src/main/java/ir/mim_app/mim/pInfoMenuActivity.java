52bbbd795bebfb5ca33d1e83c31b8d223d96cdd5
NFP_7037 <ali.ghasemzad@gmail.com>
2017-05-12 13:06:50 +0430

    pInfoMenuAcitivty text fixed ( user's name )

--- app/src/main/java/ir/mim_app/mim/pInfoMenuActivity.java
+++ app/src/main/java/ir/mim_app/mim/pInfoMenuActivity.java
@@ -1,6 +1,8 @@
 package ir.mim_app.mim;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,8 +15,6 @@ public class pInfoMenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_p_info_menu);
 
-
-        TextView nameFamily = (TextView) findViewById(R.id.nameFamily);
         TextView Txt_more = (TextView) findViewById(R.id.Txt_more);
 
         Txt_more.setOnClickListener(new View.OnClickListener() {
@@ -25,5 +25,19 @@ public class pInfoMenuActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences sharedPreferences;
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        String name = sharedPreferences.getString("name","کاربر");
+        String family = sharedPreferences.getString("family","کاربر");
+
+        String nF = name + " " + family;
+
+        TextView nameFamily = (TextView) findViewById(R.id.nameFamily);
+        nameFamily.setText(nF);
     }
 }
